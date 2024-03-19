aed0959fb28d03bd6a15d4f04ee97320072c117d
Igwbriel <gabrieldias7820@gmail.com>
2024-03-19 11:25:46 -0300

    salvando usuários em memória

--- src/server.js
+++ src/server.js
@@ -1,6 +1,27 @@
 import http from 'node:http'
 
-const server = http.createServer((req, res) => {
+const users = []
+
+const server = http.createServer((req, res) => {  
+  const {method, url} = req
+  
+  if (method === 'GET' && url=== '/users/') {
+    return res
+    .setHeader('content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url=== '/users/') {
+    users.push({
+      id: 1,
+      name: 'Isa',
+      email: 'IsaApex@gmail.com',
+    })
+
+    return res.end('Criação de úsuarios')
+  }
+
+
   return res.end('hello world');
 })
 
