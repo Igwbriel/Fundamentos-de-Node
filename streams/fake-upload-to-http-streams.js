4e329c83e45ad4e36d9121a0ce7be3bb3899d14d
Igwbriel <gabrieldias7820@gmail.com>
2024-03-22 10:59:09 -0300

    consumindo uma stream completa

--- streams/fake-upload-to-http-streams.js
+++ streams/fake-upload-to-http-streams.js
@@ -7,18 +7,23 @@ class OneToHundredStream extends Readable {
     const i = this.index++
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
-  
+
         this.push(buf)
       }
-    }, 1000)
+    }, 1000);
   }
 }
 
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
+  duplex: 'half',
+}).then(res => {
+  return res.text()
+}).then(data => {
+  console.log(data)
 })
