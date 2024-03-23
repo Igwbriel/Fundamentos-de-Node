957119dae6c59dfb9223078db5e29f3c03c396bf
Igwbriel <gabrieldias7820@gmail.com>
2024-03-23 18:46:10 -0300

    criando id unico e universal (UUID)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
 import { Database } from './database..js'
 
@@ -20,7 +21,7 @@ const server = http.createServer(async (req, res) => {
 
   if (method === 'POST' && url === '/users') {
     const user = {
-      id: 1,
+      id: randomUUID(),
       name, 
       email,
     }
